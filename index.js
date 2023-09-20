ca47a6979fc0cea7dd94922c1854076e80c5b0c2
monthonkachana <maxmonthonkachana2@gmail.com>
2023-09-20 15:36:00 +0700

    Done

--- index.js
+++ index.js
@@ -2,23 +2,52 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
 app.use(cors());
-
+app.use(express.urlencoded({ extended: true }))
 app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
-
-app.listen(port, function() {
+const originalUrls = []
+const shorUrls = []
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url
+  const foundIndex = originalUrls.indexOf(url)
+  if (!url.includes("https://") && !url.includes("https://")) {
+    return res.json({
+      error: 'invaild url'
+    })
+  }
+  if (foundIndex < 0) {
+    originalUrls.push(url)
+    shorUrls.push(shorUrls.length)
+    return res.json({
+      original_url: url,
+      short_url: shorUrls.length - 1
+    })
+  }
+  return res.json({
+    original_url: url,
+    short_url: shorUrls[foundIndex]
+  })
+})
+app.get("/api/shorturl/:shorturl", (req, res) => {
+  const shorturl = parseInt(req.params.shorturl)
+  const foundIndex = originalUrls.indexOf(shorturl)
+  if (foundIndex < 0) {
+    return res.json({
+      "error": "No short URL found for the given input"
+    })
+  }
+  res.redirect(originalUrls[foundIndex])
+})
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
